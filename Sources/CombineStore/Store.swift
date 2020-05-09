3a47ed867aacc383dd099836d978e11914a5ccc5
Jason Prasad <jasongprasad@gmail.com>
2020-05-09 13:40:08 -0400

    Stores in cancellableSet

--- Sources/CombineStore/Store.swift
+++ Sources/CombineStore/Store.swift
@@ -1,6 +1,6 @@
 import Combine
 
-class Effect<S: Equatable, A> {
+final class Effect<S: Equatable, A> {
     private var cancellable: AnyCancellable!
     private(set) var effect: Effect!
 
@@ -24,8 +24,8 @@ class Effect<S: Equatable, A> {
     typealias Effect = (AnyPublisher<A, Never>, AnyPublisher<S, Never>) -> AnyPublisher<A, Never>?
 }
 
-class Store<S: Equatable, A> {
-    private var cancellables = [AnyCancellable]()
+final class Store<S: Equatable, A> {
+    private var cancellableSet: Set<AnyCancellable> = []
     let dispatch = PassthroughSubject<A, Never>()
     let effects: [Effect<S, A>]
     let state: AnyPublisher<S, Never>
@@ -35,25 +35,25 @@ class Store<S: Equatable, A> {
          effects: [Effect<S, A>] = []) {
         let state = CurrentValueSubject<S, Never>(initialState)
         self.state = state.removeDuplicates().eraseToAnyPublisher()
-        cancellables.append(dispatch
+        dispatch
             .scan(initialState, accumulator)
             .sink(receiveValue: {
                 state.send($0)
-            }))
+            })
+            .store(in: &cancellableSet)
         // Note, this could potentially be dangerous as it leads to a circular publisher. In RxJS and RxSwift, a
         // circular publisher may need to be scheduled on another queue. An Effect that simply returns `dispatch` will
         // result in infinite recursion.
         self.effects = effects
-        cancellables.append(
-            Publishers.MergeMany(
-                effects.compactMap({
-                    $0.effect(dispatch.eraseToAnyPublisher(), self.state)
-                })
-            )
-                .sink(receiveValue: { [weak self] in
-                    self?.dispatch.send($0)
-                })
+        Publishers.MergeMany(
+            effects.compactMap({
+                $0.effect(dispatch.eraseToAnyPublisher(), self.state)
+            })
         )
+            .sink(receiveValue: { [weak self] in
+                self?.dispatch.send($0)
+            })
+            .store(in: &cancellableSet)
     }
 
     typealias Accumulator = (S, A) -> S
