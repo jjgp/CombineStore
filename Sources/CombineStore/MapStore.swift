c717f1566be9ae47053d93abc8fe0721c9dd87ef
Jason Prasad <jasongprasad@gmail.com>
2020-05-10 14:39:25 -0400

    Adds StoreProvider and MapStore

--- Sources/CombineStore/MapStore.swift
+++ Sources/CombineStore/MapStore.swift
@@ -1,1 +1,29 @@
 import SwiftUI
+
+public struct StoreProvider<S, A, Content>: View where Content: View {
+    private let content: (Store<S, A>) -> Content
+    @Environment(\.store) private var store
+
+    public init(@ViewBuilder content: @escaping (Store<S, A>) -> Content) {
+        self.content = content
+    }
+
+    public var body: some View {
+        content(store as! Store<S, A>)
+    }
+}
+
+public struct MapStore<T, S, A, Content>: View where Content: View {
+    private let content: (T) -> Content
+    @Environment(\.store) private var store
+    private let transform: (Store<S, A>) -> T
+
+    public init(_ transform: @escaping (Store<S, A>) -> T, @ViewBuilder content: @escaping (T) -> Content) {
+        self.content = content
+        self.transform = transform
+    }
+
+    public var body: some View {
+        content(transform(store as! Store<S, A>))
+    }
+}
